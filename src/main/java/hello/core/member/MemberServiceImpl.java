b1d218fa14e71abc2d34b1efca1a59079727b67a
ohyaelim <npr05080@gmail.com>
2021-01-20 22:19:21 +0900

    Use Config

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,11 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     /*
     * private final MemberRepository memberRepository 이것만 하면 구현이 안돼.
     * 인터페이스일 뿐이니까. 그래서 미리 만들어놓은 임의의 구현체 MemoryMemberRepository 로 new 해주자
